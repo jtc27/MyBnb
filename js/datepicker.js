a24b9f52a769aefca7450d826617862a3a982590
Jacob Cho <jacobcho@live.com>
2023-01-27 19:55:59 -0700

    date picker

---
+++ js/datepicker.js
@@ -0,0 +1,19 @@
+
+//  https://gijgo.com/datepicker/example/daterangepicker
+
+let today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+$('#startDate').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: today,
+    maxDate: function () {
+        return $('#endDate').val();
+    }
+});
+$('#endDate').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: function () {
+        return $('#startDate').val();
+    }
+});
